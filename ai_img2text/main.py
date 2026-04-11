a54009256c61cd5cb3f9ba3db2117d66cb82ca68
N1k78 <nikita.d4444@gmail.com>
2026-04-11 13:19:23 +0300

    first text reader

--- ai_img2text/main.py
+++ ai_img2text/main.py
@@ -1,16 +1,27 @@
-from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import logging
 from PIL import Image
+import pytesseract
 
-# Загружаем модель
-processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-sroie")
-model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-sroie")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
 
-# Открываем кусок изображения
-image = Image.open("chunk.png")
+logger = logging.getLogger(__name__)
 
-# OCR
-pixel_values = processor(images=image, return_tensors="pt").pixel_values
-generated_ids = model.generate(pixel_values)
-text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+if __name__ == "__main__":
+    logger.info("\nstart")
+    image_path = "img/image.png"
 
-print(text)
+    try:
+        image = Image.open(image_path).convert("RGB")
+        text = pytesseract.image_to_string(image, lang="eng")  # можно указать "rus" для русского
+
+    except FileNotFoundError:
+        logger.critical(f"no such file {image_path}")
+    except Exception as e:
+        logger.error(f"Другая ошибка: {e}")
+    else:
+        print(text)
+    finally:
+        logger.info("\nend")
